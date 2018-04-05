bb986813d16f7461f554ff28cb6d5cf8465b7519
ShengFQ <shengfu.qiang@163.com>
2018-04-05 23:27:21 +0800

    add log in  and log up

---
+++ app/src/main/java/com/dolores/store/http/LoginRequest.java
@@ -0,0 +1,8 @@
+package com.dolores.store.http;
+
+/**
+ * Created by sheng on 18/3/30.
+ */
+
+public class LoginRequest extends NdRequest {
+}
---
+++ app/src/main/java/com/dolores/store/util/LogUtils.java
@@ -0,0 +1,8 @@
+package com.dolores.store.util;
+
+/**
+ * Created by sheng on 18/4/5.
+ */
+
+public class LogUtils {
+}
